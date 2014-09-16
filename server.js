b7f07a94d1b35145356746fd60cab43c15e089fd
jacksongeller <jacksongeller@gmail.com>
2014-09-16 17:01:42 -0400

    mocked up:

--- server.js
+++ server.js
@@ -8,4 +8,48 @@ client.on('connect', function() {
   console.log('Redis connected');
 });
 
+var shortenerUrl = 'whatever the url is for this site';
+
+app.get('/:link', function(req, res) {
+  if (!req.params) {
+    res.send('You need specify a link');
+  }
+
+  var link = req.params.link;
+
+  client.get(link, function(err, reply) {
+    if (err) {
+      res.send('There was an error')
+    }
+    if (!reply) {
+      res.send('Could not find that link');
+    }
+
+    var url = [shortenerUrl, reply].join('');
+    res.redirect(url);
+  });
+
+});
+
+app.post('/:link', function(req, res) {
+  if (!req.params) {
+    res.send('You need specify a link');
+  }
+  var link = req.params.link;
+
+  client.get(link, function(err, reply) {
+    if (err) {
+      res.send('There was an error')
+    }
+    if (!reply) {
+      client.set(link, shortenerUrl);
+      res.redirect('/')
+    }
+
+    var url = [shortenerUrl, reply].join('');
+    res.redirect(url);
+  });
+});
+
+
 app.listen(3000);
